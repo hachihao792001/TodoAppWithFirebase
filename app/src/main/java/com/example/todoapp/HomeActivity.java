246994626bc79cb83396af1b4b8d17322fdff295
ngoctinvo <86099211+ngoctinvo@users.noreply.github.com>
2021-11-21 23:33:24 +0700

    create Spinner to choose a song

--- app/src/main/java/com/example/todoapp/HomeActivity.java
+++ app/src/main/java/com/example/todoapp/HomeActivity.java
@@ -23,6 +23,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -287,11 +288,11 @@ public class HomeActivity extends AppCompatActivity {
                     }
                     case 5: {  //relax
                         View relaxingInputDetail = inflater.inflate(R.layout.relaxing_input_detail, null);
-                        //Spinner spinner = (Spinner) relaxingInputDetail.findViewById(R.id.playlistsSpinner);
-                        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(HomeActivity.this,
-                        //       R.array.list_song, android.R.layout.simple_spinner_item);
-                        // adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        //   spinner.setAdapter(adapter);
+                        Spinner spinner = (Spinner) relaxingInputDetail.findViewById(R.id.chooseSongSpinner);
+                        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(HomeActivity.this,
+                               R.array.list_song, android.R.layout.simple_spinner_item);
+                         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                           spinner.setAdapter(adapter);
                      /*  spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                             @Override
                             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
