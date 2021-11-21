2f68f9abceec165dd7e2cb9e4a8f651407e63f79
ngoctinvo <86099211+ngoctinvo@users.noreply.github.com>
2021-11-21 23:51:49 +0700

    play chosen song (not sure can listen)

--- app/src/main/java/com/example/todoapp/RelaxingTaskDetail.java
+++ app/src/main/java/com/example/todoapp/RelaxingTaskDetail.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 public class RelaxingTaskDetail extends AppCompatActivity {
     MediaPlayer mediaPlayer;
     RelaxingTask thisTask;
+    String song;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +23,58 @@ public class RelaxingTaskDetail extends AppCompatActivity {
         TextView taskTextView = findViewById(R.id.task);
         TextView descTextView = findViewById(R.id.description);
         TextView dateTextView = findViewById(R.id.date);
-        TextView songName= findViewById(R.id.songName);
+        TextView songName = findViewById(R.id.songName);
         taskTextView.setText(thisTask.getTask());
         descTextView.setText(thisTask.getDescription());
         dateTextView.setText(thisTask.getDate());
         songName.setText(thisTask.getPlaylistName());
-    }
-    public void onMusicStart(View view){
-       mediaPlayer = MediaPlayer.create(this,R.raw.weigtless);
-       mediaPlayer.start();
+        song = songName.toString();
     }
 
-    public void onMusicPause(View view){
+
+    public void onMusicStart(View view) {
+        switch (song) {
+            case "Weightless - Marconi Union":
+                mediaPlayer = MediaPlayer.create(this, R.raw.weigtless);
+                break;
+            case "Electra - Airstream":
+                mediaPlayer = MediaPlayer.create(this, R.raw.electra);
+                break;
+            case "Mellomaniac (Chill Out Mix) - DJ Shah":
+                mediaPlayer = MediaPlayer.create(this, R.raw.mellomaniac);
+                break;
+            case "Watermark - Enya":
+                mediaPlayer = MediaPlayer.create(this, R.raw.watermark);
+                break;
+            case "Strawberry Swing - Coldplay":
+                mediaPlayer = MediaPlayer.create(this, R.raw.strawberry_swing);
+                break;
+            case "Please Dont Go - Barcelona":
+                mediaPlayer = MediaPlayer.create(this, R.raw.please_don_t_go);
+                break;
+            case "Pure Shores - All Saints":
+                mediaPlayer = MediaPlayer.create(this, R.raw.pure_shores);
+                break;
+            case "Someone Like You - Adele":
+                mediaPlayer = MediaPlayer.create(this, R.raw.someone_like_you);
+                break;
+            case "We Can Fly - Rue du Soleila":
+                mediaPlayer = MediaPlayer.create(this, R.raw.we_can_fly);
+                break;
+            default:
+                mediaPlayer = MediaPlayer.create(this, R.raw.someone_like_you);
+                break;
+        }
+        mediaPlayer = MediaPlayer.create(this, R.raw.someone_like_you);
+        mediaPlayer.start();
+    }
+
+    public void onMusicPause(View view) {
         mediaPlayer.pause();
 
     }
 
-    public void onMusicStop(View view){
+    public void onMusicStop(View view) {
         mediaPlayer.stop();
     }
 }
